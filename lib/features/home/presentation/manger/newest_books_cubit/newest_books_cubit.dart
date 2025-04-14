441e1a4988fd6214eff5cb0ee3d608428b1d2f4a
Moaz Ayman <moazayman128@gmail.com>
2025-04-14 15:12:09 +0200

    create newest books cubit state

--- lib/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -1,5 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newest_books_state.dart';
 
