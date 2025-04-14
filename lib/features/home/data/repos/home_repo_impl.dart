68032824d1c0631050dfbf7c1ada40e5f51268a9
Moaz Ayman <moazayman128@gmail.com>
2025-04-14 14:55:49 +0200

    create featured books cubit state

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -37,7 +37,28 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var date = await apiService.get(
+          endPoint: "volumes?Filtering=free-ebooks&q=subject:Programming");
+
+      List<BookModel> books = [];
+      for (var item in date["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
